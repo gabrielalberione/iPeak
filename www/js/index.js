a69934e04b4112a34bbacbacd0cc9a06ba51c4a6
gabrielalberione <gabrielalberione@gmail.com>
2015-07-16 17:59:10 -0300

    demo

--- www/js/index.js
+++ www/js/index.js
@@ -29,3 +29,24 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+// cuando devuelve la pos el gps
+var onSuccessGPS = function(position) {    
+   /* alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');*/
+    
+    puntoGPS(position.coords.longitude, position.coords.latitude);
+};
+
+// onError Callback receives a PositionError object
+//
+function onErrorGPS(error) {
+   /* alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');*/
+}
